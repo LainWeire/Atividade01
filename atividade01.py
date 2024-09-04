763332e7110252328d55b26caee558b47b77a134
LainWeire <lucasfrota3000@gmail.com>
2024-09-04 14:43:26 -0300

    first commit

---
+++ atividade01.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+x = int(input("1 valor"))
+y = int(input("2 valor"))
+soma = (x + y)
+
+print(f"a soma do valor é = {soma}")
